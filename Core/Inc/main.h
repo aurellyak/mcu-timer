8264968b37f5a31cb22599dad27178641c620555
Syifa Alifiya <145785015+cheepi@users.noreply.github.com>
2025-05-22 12:52:45 +0700

    Update main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -1,69 +1,21 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
 #define __MAIN_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+// definisi pin yang dipake
+#define LED1_Pin        GPIO_PIN_5
+#define LED1_GPIO_Port  GPIOA
+#define LED2_Pin        GPIO_PIN_6
+#define LED2_GPIO_Port  GPIOA
+#define MODE_BTN_Pin    GPIO_PIN_0
+#define MODE_BTN_Port   GPIOA
 
-/* USER CODE END Includes */
+// variabel global buat nyimpen mode aktif sekarang
+extern uint8_t mode;
 
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
-/* Exported functions prototypes ---------------------------------------------*/
+// deklarasi fungsi
+void SystemClock_Config(void);
 void Error_Handler(void);
-
-/* USER CODE BEGIN EFP */
-
-/* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
-
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* __MAIN_H */
